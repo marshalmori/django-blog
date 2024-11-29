a80972bcf816eb10dc9fbc59d9844f07b76b6d90
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-11-29 19:01:40 +0100

    Add a user relationship to a Django model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 import re
 
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -26,6 +27,8 @@ class Article(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="articles")
+
 
     def save(self, *args, **kwargs):
         text = re.sub(r"<[^>]*>", "", self.content).replace("&nbsp;", " ")
